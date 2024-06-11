9a1542bcb96a1b027ed604e28b8a1ca13d5fc558
byunjaeil <byunjaeil@gmail.com>
2024-06-11 18:22:36 +0900

    Revert "ok"
    This reverts commit 40b49c03682d2c4bb7aeb936dfff5b9e9172b9d8.

--- Apps/EmuCleaner/launch.sh
+++ Apps/EmuCleaner/launch.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 PATH="/mnt/SDCARD/System/bin:$PATH"
 
-echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 1416000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-
 silent=false
 for arg in "$@"; do
   if [ "$arg" = "-s" ]; then
@@ -54,7 +51,7 @@ for subfolder in $EmuFolder/*/; do
 
     # Build the find command with extensions from extlist
     if [ -z "$ExtList" ] || [ "$ExtList" = "null" ]; then
-      find_cmd="find \"$RomsFolder/$RomFolderName\" '!' -name '*.db' '!' -name '.gitkeep' '!' -name '*.launch' -mindepth 1 -maxdepth 2"
+      find_cmd="find \"$RomsFolder/$RomFolderName\" '!' -name '*.db' '!' -name '.gitkeep' '!' -name '*.launch' -mindepth 1 -maxdepth 1"
     else
       set -- $(echo $ExtList | tr '|' ' ')
       find_cmd="find \"$RomsFolder/$RomFolderName\""
